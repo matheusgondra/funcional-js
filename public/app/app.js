87be7a6ae3c7048d5fbc957a871b441ed0df63a2
Matheus de Gondra <matheusgondra951@gmail.com>
2023-10-16 21:32:33 -0300

    create log function

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,12 @@
-import { handleStatus } from "./utils/promises-helpers.js";
+import { handleStatus, log } from "./utils/promises-helpers.js";
 
 document.querySelector("#myButton").onclick = () => {
 	fetch("/notas")
 		.then(handleStatus)
-		.then(notas => console.log(notas))
+		.then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+		.then(log)
+		.then(itens => itens.filter(item => item.codigo == "2143"))
+		.then(itens => itens.reduce((total, item) => total + item.valor, 0))
+		.then(log)	
 		.catch(err => console.log(err));
 }
--- public/app/utils/promises-helpers.js
+++ public/app/utils/promises-helpers.js
@@ -1,3 +1,8 @@
 export const handleStatus = res => {
 	return res.ok ? res.json() : Promise.reject(res.statusText);
 }
+
+export const log = param => {
+	console.log(param);
+	return param;
+}
