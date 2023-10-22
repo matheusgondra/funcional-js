f6e4a2f639e7f4c701a4de2e15b8ecbad3b9f63d
Matheus de Gondra <matheusgondra951@gmail.com>
2023-10-22 11:26:35 -0300

    create operations function

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,18 @@
 import { log } from "./utils/promises-helpers.js";
 import "./utils/array-helpers.js";
 import { notasService as service } from "./nota/service.js";
-import { takeUntil, debounceTime } from "./utils/operators.js";
+import { takeUntil, debounceTime, partialize, pipe } from "./utils/operators.js";
 
-const action = debounceTime(500, takeUntil(3, () => 
+const operations = pipe(
+	partialize(takeUntil, 3),
+	partialize(debounceTime, 500)
+);
+
+const action = operations(() => 
 	service
 		.sumItems("2143")
 		.then(log)
 		.catch(err => console.log(err))
-));
+);
 
 document.querySelector("#myButton").onclick = action;
