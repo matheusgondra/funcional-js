2517cb842267198900def91b82962f6d8175a2bb
Matheus de Gondra <matheusgondra951@gmail.com>
2023-10-22 11:34:27 -0300

    create timeoutPromise function

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-import { log } from "./utils/promises-helpers.js";
+import { log, timeoutPromise } from "./utils/promises-helpers.js";
 import "./utils/array-helpers.js";
 import { notasService as service } from "./nota/service.js";
 import { takeUntil, debounceTime, partialize, pipe } from "./utils/operators.js";
@@ -9,8 +9,7 @@ const operations = pipe(
 );
 
 const action = operations(() => 
-	service
-		.sumItems("2143")
+	timeoutPromise(200, service.sumItems("2143"))
 		.then(log)
 		.catch(err => console.log(err))
 );
--- public/app/utils/promises-helpers.js
+++ public/app/utils/promises-helpers.js
@@ -6,3 +6,10 @@ export const log = param => {
 	console.log(param);
 	return param;
 }
+
+export const timeoutPromise = (miliseconds, promise) => {
+	const timeout = new Promise((resolve, reject) => 
+		setTimeout(() => reject(`Limite da promise excedido (limite: ${miliseconds} ms)`), miliseconds));
+	
+	return Promise.race([timeout, promise]);
+}
