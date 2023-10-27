bb482dcec53ad914a172a69eee9967320b572a05
Matheus de Gondra <matheusgondra951@gmail.com>
2023-10-27 19:45:29 -0300

    create EventEmitter

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,7 @@ import { delay, log, retry, timeoutPromise } from "./utils/promises-helpers.js";
 import "./utils/array-helpers.js";
 import { notasService as service } from "./nota/service.js";
 import { takeUntil, debounceTime, partialize, pipe } from "./utils/operators.js";
+import { EventEmitter } from "./utils/event-emitter.js";
 
 const operations = pipe(
 	partialize(takeUntil, 3),
@@ -11,7 +12,7 @@ const operations = pipe(
 const action = operations(() => 
 	retry(3, 3000, () => timeoutPromise(200, service.sumItems("2143")))
 		.then(delay(5000))
-		.then(log)
+		.then(total => EventEmitter.emit("itensTotalizados", total))
 		.catch(err => console.log(err))
 );
 
