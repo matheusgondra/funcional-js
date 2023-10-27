86d220a5311c2284918f3daa460919154e7ac9e3
Matheus de Gondra <matheusgondra951@gmail.com>
2023-10-27 19:58:21 -0300

    using Monoda

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,11 +1,12 @@
-import { compose, partialize, pipe } from "../utils/operators.js";
+import { Maybe } from "../utils/maybe.js";
+import { partialize, pipe } from "../utils/operators.js";
 import { handleStatus } from "../utils/promises-helpers.js";
 
 const API = "http://localhost:3000/notas";
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM => notasM.map(notas => notas.$flatMap(nota => nota.itens));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.codigo === code));
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 const sumItems = code => notas => notas
 	.$flatMap(nota => nota.itens)
@@ -16,6 +17,7 @@ export const notasService = {
 	listAll() {
 		return fetch(API)
 			.then(handleStatus)
+			.then(notas => Maybe.of(notas))
 			.catch(err => {
 				console.log(err);
 				return Promise.reject("Não foi possível obter as notas fiscais");
@@ -28,6 +30,8 @@ export const notasService = {
 			filterItems,
 			sumItemsValue
 		);
-		return this.listAll().then(sumItems)
+		return this.listAll()
+			.then(sumItems)
+			.then(result => result.getOrElse(0));
 	}
 }
